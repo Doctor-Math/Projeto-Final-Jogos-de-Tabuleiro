3a513cce3a79f8cd9e1d3de9de7bee38ed0da721
renatovalis <renatolucasvalis@gmail.com>
2025-01-18 20:12:32 -0300

    Implementada parte da função que verifica a vitória no ligue 4: linhas e colunas

--- src/Lig4.cpp
+++ src/Lig4.cpp
@@ -1,57 +1,112 @@
 #include <iostream>
 #include "Lig4.hpp"
 
-const int TAM_LINHAS = 6, TAM_COL=7;
+const int TAM_LINHAS = 6, TAM_COL = 7;
 
-Lig4::Lig4(){
-    this->dimensionarTabuleiro(6,7);
+Lig4::Lig4()
+{
+    this->dimensionarTabuleiro(6, 7);
 }
 
-void Lig4::validarJogada(int coluna, int turno){
-    int flag=0;
+void Lig4::validarJogada(int nulo, int coluna, int turno)
+{
+    int flag = 0;
     coluna--;
-    for(int i=(TAM_LINHAS-1);i>=0;i--){
-        if(this->retornarPosicao(i,coluna)=='\0'){
-            this->marcarTabuleiro(i,coluna,(turno==0)? 'X':'O');
-            flag=1;
+    for (int i = (TAM_LINHAS - 1); i >= 0; i--)
+    {
+        if (this->retornarPosicao(i, coluna) == '\0')
+        {
+            this->marcarTabuleiro(i, coluna, (turno == 0) ? 'X' : 'O');
+            flag = 1;
             break;
         };
     };
-    if(!flag){
-        std::cout<<"ERRO: jogada inválida";
+    if (!flag)
+    {
+        std::cout << "ERRO: jogada inválida";
     };
 };
 
-void Lig4::imprimirTabuleiro(){
-    for(int i=0;i<TAM_LINHAS;i++){
-        std::cout<<"|";
-        for(int j=0;j<TAM_COL;j++){
-            std::cout<<this->retornarPosicao(i,j)<<"|";
+void Lig4::imprimirTabuleiro()
+{
+    for (int i = 0; i < TAM_LINHAS; i++)
+    {
+        std::cout << "|";
+        for (int j = 0; j < TAM_COL; j++)
+        {
+            std::cout << this->retornarPosicao(i, j) << "|";
         };
-        std::cout<<std::endl;
+        std::cout << std::endl;
     };
 };
 
-int Lig4::verificarVitoria(char marcacao){
+int verificarVitoria(char marcacao)
+{
     int flag = 0;
-
-    //Verificar nas linhas 
-    for(int i=0;i<TAMANHO;i++){
-       int j = (TAM_COL - 1)/2 
-            if(this->retornarPosicao(i,j)==marcacao){
-                flag=1;
-                if(j==2){
-                    return flag;
-                }
-            }else{
-                flag=0;
-                break;
+    int i, j;
+    // Verifica as linhas:
+    for (i = 0; i++; i < TAM_LINHAS)
+    {
+        for (j = 0; j++; j < TAM_COL)
+        {
+            if (this->retornarPosicao(i, j) == marcacao)
+            {
+                flag++;
+                if (flag == 4)
+                {
+                    return 1;
+                };
+            }
+            else
+            {
+                flag = 0;
             };
         };
     };
 
+    // Verifica as colunas:
+    for (j = 0; j++; j < TAM_COL)
+    {
+        for (i = 0; i++; i < TAM_LINHAS)
+        {
+            if (this->retornarPosicao(i, j) == marcacao)
+            {
+                flag++;
+                if (flag == 4)
+                {
+                    return 1;
+                };
+            }
+            else
+            {
+                flag = 0;
+            };
+        };
+    };
+}
+
+/*
+Verifica se o meio corresponde:
+    se sim:
+        verifica se meio + 1, meio+2 e meio são iguais:
+            se sim:
+                verifica se
+
+
+verifica se o meio corresponde:
+    se sim:
+        verifica se meio + 2 corresponde:
+            se sim:
+                verifica se meio+1 e meio+3 corespondem:
+                    se sim:
+                        retorna vitória
+                    se não:
+                        retorna 0
+            se não:
+                retona 0
+    se não:
+        retorna 0
 
 
 
-
-    
+*/
