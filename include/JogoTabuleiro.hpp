77ad540f3cf63f75d422cf0a2becd32fd2f7e4d4
renatovalis <165953868+renatovalis@users.noreply.github.com>
2025-01-28 22:22:43 -0300

    Update JogoTabuleiro.hpp

--- include/JogoTabuleiro.hpp
+++ include/JogoTabuleiro.hpp
@@ -4,20 +4,23 @@
 #include <iostream>
 #include <vector>
 
-class JogoTabuleiro{
-    private:
+class JogoTabuleiro
+{
+private:
     std::vector<std::vector<char>> tabuleiro;
 
-    public:
+public:
     JogoTabuleiro();
     void dimensionarTabuleiro(int, int);
-    void marcarTabuleiro(int,int,char);
-    char retornarPosicao(int,int);
-    virtual void validarJogada(int,int, int)=0;
-    virtual int verificarVitoria(char)=0;
-    virtual void imprimirTabuleiro()=0;
-    virtual ~JogoTabuleiro();
+    void marcarTabuleiro(int, int, char);
+    char retornarPosicao(int, int);
+    virtual void validarJogada(int, int, int) = 0;
 
+    virtual int verificarVitoria(char);
+    virtual int verificarVitoria(char, int, int);
+
+    virtual void imprimirTabuleiro() = 0;
+    virtual ~JogoTabuleiro();
 };
 
 #endif
