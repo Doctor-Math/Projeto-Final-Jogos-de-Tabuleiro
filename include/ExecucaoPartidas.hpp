2d68924f4784bd8f11f4827ebf40184c63bfa9fa
renatovalis <165953868+renatovalis@users.noreply.github.com>
2025-01-28 22:21:28 -0300

    Update ExecucaoPartidas.hpp

--- include/ExecucaoPartidas.hpp
+++ include/ExecucaoPartidas.hpp
@@ -11,9 +11,9 @@
 
 void escreverJogo(std::ofstream &arq_temp, int i, int vitorias, int derrotas);
 void pontuarJogadores(std::string vencedor, std::string perdedor, int linha_modificar);
-void partidaVelha(std::string apelidoJ1, std::string apelidoJ2, JogoDaVelha* &tabuleiro);
-void partidaLig4(std::string apelidoJ1, std::string apelidoJ2,Lig4* &tabuleiro);
-void partidaReversi(std::string apelidoJ1, std::string apelidoJ2,Reversi* &tabuleiro);
-void executarPartida(char tipoJogo,std::string apelidoJ1,std::string apelidoJ2);
+void partidaVelha(std::string apelidoJ1, std::string apelidoJ2, JogoDaVelha *&tabuleiro);
+void partidaLig4(std::string apelidoJ1, std::string apelidoJ2, Lig4 *&tabuleiro);
+void partidaReversi(std::string apelidoJ1, std::string apelidoJ2, Reversi *&tabuleiro);
+void executarPartida(char tipoJogo, std::string apelidoJ1, std::string apelidoJ2);
 
 #endif
